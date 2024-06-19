ab4f5e486838e05ed785ec77b3ad73176aaf8e9d
andersonlimadev <andersonlimahw@gmail.com>
2024-06-19 00:13:26 -0300

    🐞 fix: build e moduleResolution

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lm5o3nhugt8"
+    "revision": "0.osaklj5jn1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
