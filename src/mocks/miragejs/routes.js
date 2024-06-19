0cd26acf2864398140062c807e069ac8174b6573
andersonlimadev <andersonlimahw@gmail.com>
2024-06-19 00:17:02 -0300

    🆕 feature: atualizando miragejs

--- src/mocks/miragejs/routes.js
+++ src/mocks/miragejs/routes.js
@@ -21,59 +21,6 @@ createServer({
     // specifying the fully qualified domain name
     this.passthrough("http://api.acme.com/**");
 
-    // Sample with json
-    // return simple json copiyng from api
-    this.get('/people', (schema, request) => {
-      const data = peopleMock;
-      console.log('Miragejs: GET people schema: ', schema, ' request: ', request);
-      return new Response(200, headers, data);
-    });
-
-    this.get('/people/:id' + PEOPLE_BY_ID_ROUTE, (schema, request) => {
-      const [ personById ] = peopleMock;
-      const data = personById;
-      return new Response(200, headers, data);
-    });
-
-    // Sample with models:
-    // return dynamisc models based on miragejs
-
-    this.post('/' + PEOPLE_ROUTE, (schema, request) => {
-      let body = JSON.parse(request.requestBody);
-      const data = {
-        id: 2
-      };
-
-      return new Response(201, headers, data);
-    });
-
-    this.put('/' + PEOPLE_BY_ID_ROUTE, (schema, request) => {
-      const { id } = request.params;
-      let people = peopleMock.find((x) => x.id === id);
-      if (!people) {
-        return new Response(404, headers, {
-          message: `People with id ${id} not found!`,
-        });
-      }
-
-      const data = {};
-
-      return new Response(204, headers, data);
-    });
-
-    
-
-    this.delete('/' + PEOPLE_BY_ID_ROUTE, (schema, request) => {
-      const { id } = request.params;
-      let people = peopleMock.find((x) => x.id === id);
-      if (!people) {
-        return new Response(404, headers, {
-          message: `People with id ${id} not found!`,
-        });
-      }
-      const data = {};
-      return new Response(204, headers, data);
-    });
 
     // Meli:
     this.get('/departments', (schema, request) => {
